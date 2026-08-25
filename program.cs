5a6b6ab88a216c385afd31698805016c5e28e753
Jade <76448871+Sushii64@users.noreply.github.com>
2026-08-25 13:51:02 -0600

    m

---
+++ program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sushi's consoleapp version 1.3
+            Helpers.Pause();
+        }
+    }
+}
